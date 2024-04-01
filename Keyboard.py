59c928a5047ed4fcd6f74572522a0a58cf666501
Байкин Егор <Nout5f421@hotmail.com>
2024-04-01 23:44:03 +0300

    Update: Изменина клавиатура

--- Keyboard.py
+++ Keyboard.py
@@ -4,5 +4,7 @@ from aiogram.types import FSInputFile
 
 async def hello_btn(callback: types.CallbackQuery):
     # 🫘 🐟
-    await callback.message.answer_photo(photo=FSInputFile('Stickers/hello.gif'),
-                                        caption="Привет '{name}'".format(name=callback.message.chat.first_name))
+    await callback.message.answer_photo(photo=FSInputFile('Stickers/hello.jpg'),
+                                        caption="Спасибо запомню '{name}'".format(
+                                            name=callback.message.chat.first_name
+                                        ))
